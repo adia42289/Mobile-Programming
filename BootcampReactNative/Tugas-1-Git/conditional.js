3d03d162fa0066e12e724aa3e44c96221a399743
adia42289 <80579893+adia42289@users.noreply.github.com>
2021-03-16 18:54:52 +0700

    Add files via upload

---
+++ BootcampReactNative/Tugas-1-Git/conditional.js
@@ -0,0 +1,89 @@
+var nama = "Angga"
+var peran = "Penyihir"
+if (nama == "") {
+    console.log("Nama Harus Diisi!")
+} else if (peran == "") {
+    console.log("Halo Angga, Silahkan isi peran kamu untuk memulai permainan!")
+} else {
+    console.log("Selamat datang di Dunia Werewolf, Dimas\nHalo Penyihir Angga, kamu dapat melihat siapa yang menjadi werewolf!\n")
+}
+var nama = "Rudi"
+var peran = "Guard"
+if (nama == "") {
+    console.log("Nama Harus Diisi!")
+} else if (peran == "") {
+    console.log("Halo Rudi, Silahkan isi peran kamu untuk memulai permainan!")
+} else {
+    console.log("Selamat datang di Dunia Werewolf, Rudi\nHalo Guard Rudi, kamu dapat membantu melindungi temanmu dari serangan werewolf!\n")
+}
+var nama = "Ali"
+var peran = "Werewolf"
+if (nama == "") {
+    console.log("Nama Harus Diisi!")
+} else if (peran == "") {
+    console.log("Halo Ali, Silahkan isi peran kamu untuk memulai permainan!")
+} else {
+    console.log("Selamat datang di Dunia Werewolf, Ali\nHalo Werewolf Yudi, kamu dapat memangsa 1 orang setiap malam!\n")
+}
+
+console.log ("\n")
+
+var hari = 1;
+var bulan = 5;
+var tahun = 1945;
+
+switch (hari) {
+    case hari:
+        console.log(hari);
+        break;
+    default:
+}
+switch (bulan) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("Maret");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("Mei");
+        break;
+    case 6:
+        console.log("Juni");
+        break;
+    case 7:
+        console.log("Juli");
+        break;
+    case 8:
+        console.log("Agustus");
+        break;
+    case 9:
+        console.log("September");
+        break;
+    case 10:
+        console.log("Oktober");
+        break;
+    case 11:
+        console.log("November");
+        break;
+    case 12:
+        console.log("Desember");
+        break;
+    default:
+        console.log("Bulan Tidak Ada!");
+        break;
+}
+switch (tahun) {
+    case tahun:
+        console.log(tahun);
+        break;
+
+    default:
+        break;
+}
